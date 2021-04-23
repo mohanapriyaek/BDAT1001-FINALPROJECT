a024d8637418f5b4b468a0d5693bcfad57a54642
mohanapriyaek <78195492+mohanapriyaek@users.noreply.github.com>
2021-04-23 07:38:48 +0530

    Add files via upload

---
+++ ApiTwo/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using IdentityModel.Client;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace ApiTwo.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public HomeController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        [Route("/home")]
+        public async Task<IActionResult> Index()
+        {
+            //retrieve access token
+            var serverClient = _httpClientFactory.CreateClient();
+
+            var discoveryDocument = await serverClient.GetDiscoveryDocumentAsync("https://localhost:44370/");
+
+            var tokenResponse = await serverClient.RequestClientCredentialsTokenAsync(
+                new ClientCredentialsTokenRequest
+                {
+                    Address = discoveryDocument.TokenEndpoint,
+
+                    ClientId = "client_id",
+                    ClientSecret = "client_secret",
+
+                    Scope = "ApiOne",
+                });
+
+            //retrieve secret data
+            var apiClient = _httpClientFactory.CreateClient();
+
+            apiClient.SetBearerToken(tokenResponse.AccessToken);
+
+            var response = await apiClient.GetAsync("https://localhost:44349/secret");
+
+            var content = await response.Content.ReadAsStringAsync();
+
+            return Ok(new
+            {
+                access_token = tokenResponse.AccessToken,
+                message = content,
+            });
+        }
+    }
+}
